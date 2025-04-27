2b15c446ec35512366660cf381d99150d6b4ad0b
Mariam Gvenetadze <mariam.gvenetadze@duke.edu>
2025-04-27 19:18:39 -0400

    deleted old ticker list

--- fintech interactive brokers/main.py
+++ fintech interactive brokers/main.py
@@ -1,20 +1,5 @@
 from data_fetcher import fetch_historical_data, optimize_portfolio, fetch_latest_prices, fetch_fundamentals_yf
 from trader import rebalance_portfolio, get_account_value
-'''
-TICKERS = ["AAPL", "TSLA", "MSTR", "GME", "AMZN", "USO", "SHY", "IVV", "QQQ", "IBKR", "MSFT", "NVDA", "SPY", "META", "UNH", "V", "JPM", "RBLX", "DKNG", "CELH",  "SOFI", "HOOD", "MSTR", "ABNB", "SOFI",
-# 🏛️ Sector ETFs (SPDR Sector Funds - equity sectors)
-    "XLF",  # Financials
-    "XLK",  # Technology
-    "XLY",  # Consumer Discretionary
-    # "XLC",  # Communication Services
-    "XLE",  # Energy
-    "XLV",  # Health Care
-    "XLI",  # Industrials
-    "XLB",  # Materials
-    # "XLRE", # Real Estate
-    "XLU",  # Utilities]
-    "SPY", "QQQ", "IVV", "VTI", "IWM", "DIA"] 
-'''
 
 # Individual US Stocks
 STOCK_TICKERS = [
