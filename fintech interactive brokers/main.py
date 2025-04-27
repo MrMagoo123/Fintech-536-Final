c742184a46c0b66fddd7ea2f651c92cc62859b6d
tonycui <tonyyyycui@gmail.com>
2025-04-26 23:22:15 -0400

    fixed small bug

--- fintech interactive brokers/main.py
+++ fintech interactive brokers/main.py
@@ -5,7 +5,7 @@ TICKERS = ["AAPL", "TSLA", "MSTR", "GME", "AMZN", "USO", "SHY", "IVV", "QQQ", "V
 
 
 def main():
-    print(yf.__version__)
+    # print(yf.__version__)
 
     print("🔍 Fetching historical data...")
     historical_data = fetch_historical_data(TICKERS)
